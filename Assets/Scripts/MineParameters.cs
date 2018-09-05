f307adcd142ec74a1e391c2d3078b42ee52bb019
Dalius Grazinskis <grazinskisd@gmail.com>
2018-09-05 22:15:54 +0200

    [WIP] Mine selector

--- Assets/Scripts/MineParameters.cs
+++ Assets/Scripts/MineParameters.cs
@@ -5,6 +5,7 @@ namespace IdleMiner
     [CreateAssetMenu(menuName = "IdleMiner/MineParameters")]
     public class MineParameters: ScriptableObject
     {
+        public string MineName;
         public Parameters WarehouseParams;
         public Parameters LiftParams;
         public Parameters[] MineshaftParams;
